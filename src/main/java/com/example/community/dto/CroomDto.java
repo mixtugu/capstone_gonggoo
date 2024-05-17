ecb77d96c79be7afe24ebf72e5da6f5f83936653
Seo Kyeong Won <skwknh00@gmail.com>
2024-05-17 09:11:43 +0900

    mypage 4.19 4 refactor MyPageController.java , NoticeController.java

---
+++ src/main/java/com/example/community/dto/CroomDto.java
@@ -0,0 +1,68 @@
+package com.example.community.dto;
+
+import com.example.community.domain.entity.Croom;
+import com.example.domain.Member;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class CroomDto {
+    private Integer roomId;
+    private String author;
+    private Integer recruitNum;
+    private Integer currentNum;
+    private Integer roomCategory;
+    private String roomTitle;
+    private String communityCategory;
+    private String detailCategory;
+    private String region;
+    private String detailRegion;
+    private Double payment;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private Member member;
+
+
+
+    public Croom toEntity() {
+        Croom build= Croom.builder()
+                .roomId(roomId)
+                .author(author)
+                .recruitNum(recruitNum)
+                .currentNum(currentNum)
+                .roomCategory(3)
+                .roomTitle(roomTitle)
+                .communityCategory(communityCategory)
+                .detailCategory(detailCategory)
+                .region(region)
+                .detailRegion(detailRegion)
+                .payment(payment)
+
+                .member(member)
+                .build();
+        return build;
+    }
+    @Builder
+    public CroomDto(Integer roomId, String author, Integer recruitNum, Integer currentNum, Integer roomCategory,
+                    String roomTitle, String communityCategory,String detailCategory, String region, String detailRegion, /*Integer numberOfParticipants,*/ Double payment,  LocalDateTime createdDate, LocalDateTime modifiedDate, Member member) {
+        this.roomId = roomId;
+        this.author=author;
+        this.recruitNum = recruitNum;
+        this.currentNum =currentNum;
+        this.roomCategory = roomCategory;
+        this.roomTitle =roomTitle;
+        this.communityCategory =communityCategory;
+        this.detailCategory = detailCategory;
+        this.region = region;
+        this.detailRegion = detailRegion;
+        this.payment = payment;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+
+        this.member = member ;
+    }
+}
---
+++ src/main/java/com/example/community/service/CparticipantService.java
@@ -0,0 +1,85 @@
+package com.example.community.service;
+
+import com.example.domain.Member;
+import com.example.community.domain.entity.Croom;
+import com.example.community.domain.entity.Cparticipant;
+import com.example.community.domain.repository.CroomRepository;
+import com.example.community.domain.repository.CparticipantRepository;
+import com.example.community.dto.CparticipantDto;
+import com.example.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CparticipantService {
+
+    private final CparticipantRepository cparticipantRepository;
+    private final MemberRepository memberRepository;
+    private final CroomRepository croomRepository;
+
+    public CparticipantService(CparticipantRepository cparticipantRepository, MemberRepository memberRepository, CroomRepository croomRepository) {
+        this.cparticipantRepository = cparticipantRepository;
+        this.memberRepository = memberRepository;
+        this.croomRepository = croomRepository;
+    }
+
+
+    @Transactional
+    public Integer addParticipant(Integer croomId, String loginId, boolean isRoomOwner) {
+
+        Croom croom = croomRepository.findById(croomId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + croomId));
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다. loginId=" + loginId));
+
+        Cparticipant cparticipant = Cparticipant.builder()
+                .member(member)
+                .croom(croom)
+                .isRoomOwner(isRoomOwner)
+                .build();
+
+        cparticipantRepository.save(cparticipant);
+        return cparticipant.getRoomMemberId();
+    }
+
+    public List<Cparticipant> getCparticipantsByCroomId(Integer croomId) {
+        Croom croom = croomRepository.findById(croomId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + croomId));
+        return cparticipantRepository.findByCroom(croom);
+    }
+
+    public List<CparticipantDto> getCparticipantDtosByCroomId(Integer croomId) {
+        Croom croom = croomRepository.findById(croomId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. id=" + croomId));
+        List<Cparticipant> cparticipants = cparticipantRepository.findByCroom(croom);
+        return cparticipants.stream()
+                .map(CparticipantDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isUserParticipated(Integer croomId, String loginId) {
+        return cparticipantRepository.existsByCroomRoomIdAndMember_LoginId(croomId, loginId);
+    }
+
+    public boolean removeCparticipant(Integer croomId, String loginId) {
+        Cparticipant cparticipant = cparticipantRepository.findByCroomRoomIdAndMember_LoginId(croomId, loginId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 참여 정보를 찾을 수 없습니다. croomId: " + croomId + ", loginId: " + loginId));
+        cparticipantRepository.delete(cparticipant);
+        return false;
+    }
+
+    @Transactional
+    public boolean withdrawFromCroom(Integer roomId, String loginId) {
+        try {
+            Cparticipant cparticipant = cparticipantRepository.findByCroomRoomIdAndMember_LoginId(roomId, loginId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 참여 정보를 찾을 수 없습니다. boardId: " + roomId + ", loginId: " + loginId));
+
+            Croom croom = cparticipant.getCroom();
+
+            cparticipantRepository.delete(cparticipant);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
